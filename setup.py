952c5f4dd5c439034ff51fce2daa8540b4129a5e
Maniraman Periyasamy <maniraman.periyasamy@gmail.com>
2024-05-16 14:35:44 +0200

    dist linux

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gspsa-gradients',
-    version='1.0',
+    version='0.1.0',
     packages=find_packages(),
     py_modules=['gspsa-gradients'],
     install_requires=[
