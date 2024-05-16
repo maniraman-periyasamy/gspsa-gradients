8c448e76918d49504f1ae05d09b519e3459580be
Maniraman Periyasamy <maniraman.periyasamy@gmail.com>
2024-05-16 09:54:19 +0200

    TFQ and Qiskit version implemented

--- gspsa_gradients/__init__.py
+++ gspsa_gradients/__init__.py
@@ -1,3 +1,13 @@
+"""
+GSPSA-Gradients
+
+An implementation of Guided-SPSA gradient estimation technique introduced here: https://arxiv.org/abs/2404.15751
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Maniraman Periyasay'
+
+
 name='gspsa_gradients'
 
 from . import qiskit_gradient
